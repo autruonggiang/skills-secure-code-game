65f4036c6cb46b838e3beb7e413df4e192f21424
James Au <21522019@gm.uit.edu.vn>
2024-03-18 08:30:01 +0700

    fix: bug in Season-1/Level-1/hack.py

--- Season-1/Level-1/hack.py
+++ Season-1/Level-1/hack.py
@@ -1,34 +1,10 @@
-from collections import namedtuple
-import math
-
-Order = namedtuple('Order', 'id, items')
-Item = namedtuple('Item', 'type, description, amount, quantity')
-
-def validorder(order: Order):
-    net = 0
-
-    for item in order.items:
-        if item.type == 'payment':
-            net += item.amount
-        elif item.type == 'product':
-            net -= item.amount * item.quantity
-        else:
-            return "Invalid item type: %s" % item.type
-
-    # Check if the absolute difference between net and 0 is within a tolerance
-    if math.isclose(net, 0, abs_tol=0.001):
-        return "Order ID: %s - Full payment received!" % order.id
-    else:
-        return "Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net)
-
-
 import unittest
 import code as c
 
 class TestOnlineStore(unittest.TestCase):
-
     # Tricks the system and walks away with 1 television, despite valid payment & reimbursement
     def test_6(self):
+        import code as c
         tv_item = c.Item(type='product', description='tv', amount=1000.00, quantity=1)
         payment = c.Item(type='payment', description='invoice_4', amount=1e19, quantity=1)
         payback = c.Item(type='payment', description='payback_4', amount=-1e19, quantity=1)
@@ -37,6 +13,7 @@ class TestOnlineStore(unittest.TestCase):
 
     # Valid payments that should add up correctly, but do not
     def test_7(self):
+        import code as c
         small_item = c.Item(type='product', description='accessory', amount=3.3, quantity=1)
         payment_1 = c.Item(type='payment', description='invoice_5_1', amount=1.1, quantity=1)
         payment_2 = c.Item(type='payment', description='invoice_5_2', amount=2.2, quantity=1)
@@ -45,6 +22,7 @@ class TestOnlineStore(unittest.TestCase):
 
     # The total amount payable in an order should be limited
     def test_8(self):
+        import code as c
         num_items = 12
         items = [c.Item(type='product', description='tv', amount=99999, quantity=num_items)]
         for i in range(num_items):
